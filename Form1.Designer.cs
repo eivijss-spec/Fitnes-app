25905fd99caec98aa1a075096a2cd9232946f8b4
Deivijs <66945443+eivijss-spec@users.noreply.github.com>
2024-04-17 11:17:22 +0300

    1

---
+++ Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace fitnesa_aplikācija
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.B_SS = new System.Windows.Forms.Button();
+            this.rtb_taimers = new System.Windows.Forms.RichTextBox();
+            this.B_pauz = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.B_Step = new System.Windows.Forms.Button();
+            this.rtb_step = new System.Windows.Forms.RichTextBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // B_SS
+            // 
+            this.B_SS.Location = new System.Drawing.Point(63, 32);
+            this.B_SS.Name = "B_SS";
+            this.B_SS.Size = new System.Drawing.Size(75, 23);
+            this.B_SS.TabIndex = 0;
+            this.B_SS.Text = "Start";
+            this.B_SS.UseVisualStyleBackColor = true;
+            this.B_SS.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // rtb_taimers
+            // 
+            this.rtb_taimers.Location = new System.Drawing.Point(263, 12);
+            this.rtb_taimers.Name = "rtb_taimers";
+            this.rtb_taimers.Size = new System.Drawing.Size(139, 43);
+            this.rtb_taimers.TabIndex = 1;
+            this.rtb_taimers.Text = "";
+            this.rtb_taimers.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // B_pauz
+            // 
+            this.B_pauz.Location = new System.Drawing.Point(168, 32);
+            this.B_pauz.Name = "B_pauz";
+            this.B_pauz.Size = new System.Drawing.Size(75, 23);
+            this.B_pauz.TabIndex = 2;
+            this.B_pauz.Text = "pauze";
+            this.B_pauz.UseVisualStyleBackColor = true;
+            this.B_pauz.Click += new System.EventHandler(this.B_pauz_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // B_Step
+            // 
+            this.B_Step.Location = new System.Drawing.Point(99, 160);
+            this.B_Step.Name = "B_Step";
+            this.B_Step.Size = new System.Drawing.Size(75, 23);
+            this.B_Step.TabIndex = 3;
+            this.B_Step.Text = "button1";
+            this.B_Step.UseVisualStyleBackColor = true;
+            this.B_Step.Click += new System.EventHandler(this.B_Step_Click);
+            // 
+            // rtb_step
+            // 
+            this.rtb_step.Location = new System.Drawing.Point(263, 102);
+            this.rtb_step.Name = "rtb_step";
+            this.rtb_step.Size = new System.Drawing.Size(100, 96);
+            this.rtb_step.TabIndex = 4;
+            this.rtb_step.Text = "";
+            this.rtb_step.TextChanged += new System.EventHandler(this.rtb_step_TextChanged);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(493, 117);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(165, 154);
+            this.checkedListBox1.TabIndex = 5;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.rtb_step);
+            this.Controls.Add(this.B_Step);
+            this.Controls.Add(this.B_pauz);
+            this.Controls.Add(this.rtb_taimers);
+            this.Controls.Add(this.B_SS);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button B_SS;
+        private System.Windows.Forms.RichTextBox rtb_taimers;
+        private System.Windows.Forms.Button B_pauz;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button B_Step;
+        private System.Windows.Forms.RichTextBox rtb_step;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.SQLite;
+using Newtonsoft.Json;
+using System.Net.Http;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Fitness.v1;
+using Google.Apis.Fitness.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System.Threading;
+
+namespace fitnesa_aplikācija
+{
+    public partial class Form1 : Form
+    {
+
+        private bool isTimerRunning = false;
+        private bool isPaused = false;
+        private DateTime startTime;
+        private TimeSpan elapsedTime;
+        private TimeSpan pausedDuration;
+        private GoogleFitClient googleFitClient;
+        private CancellationTokenSource cancellationTokenSource;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            googleFitClient = new GoogleFitClient(); // Initialize Google Fit client
+        }
+
+
+
+        static SQLiteConnection CreateConnection()
+        {
+            SQLiteConnection Reps = new SQLiteConnection("Data Source=fitnes.db; Version=3; New=true; Compress=True;");
+            return Reps;
+        }
+
+        static SQLiteConnection CreateConeection()
+        {
+            SQLiteConnection Reps;
+            Reps = new SQLiteConnection("Data Source=fitnes.db; Version = 3; New = true; Compress = True;");
+            try
+            {
+                Reps.Open();
+
+            }
+            catch
+            {
+
+            }
+            return Reps;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!isTimerRunning)
+            {
+                // Start the timer
+                isTimerRunning = true;
+                startTime = DateTime.Now;
+                timer1.Start();
+                B_SS.Text = "Stop";
+            }
+            else
+            {
+                // Stop the timer
+                isTimerRunning = false;
+                timer1.Stop();
+                B_SS.Text = "Start";
+                UpdateTime();
+            }
+        }
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void B_pauz_Click(object sender, EventArgs e)
+        {
+            if (isTimerRunning)
+            {
+                if (!isPaused)
+                {
+                    // Pause the timer
+                    isPaused = true;
+                    timer1.Stop();
+                    B_pauz.Text = "Atsākts";
+                    pausedDuration = DateTime.Now - startTime; // Store the duration of pause
+                }
+                else
+                {
+                    // Resume the timer
+                    isPaused = false;
+                    startTime = DateTime.Now - pausedDuration; // Subtract paused duration from current time
+                    timer1.Start();
+                    B_pauz.Text = "Pauze";
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            UpdateTime();
+        }
+        private void UpdateTime()
+        {
+            elapsedTime = DateTime.Now - startTime;
+            rtb_taimers.Text = elapsedTime.ToString(@"hh\:mm\:ss");
+        }
+
+        private void rtb_step_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void B_Step_Click(object sender, EventArgs e)
+        {
+            DateTime endDate = DateTime.Now;
+            DateTime startDate = endDate.AddDays(-1); // Example: Fetch step count data for the last 24 hours
+
+
+            try
+            {
+                int stepCount = await googleFitClient.GetStepCountAsync(startDate, endDate);
+                rtb_step.Text = stepCount.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error fetching step count: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public class GoogleFitClient
+        {
+            private static string[] Scopes = { FitnessService.Scope.FitnessActivityRead };
+            private static string ApplicationName = "Your application name";
+
+            public async Task<int> GetStepCountAsync(DateTime startDate, DateTime endDate)
+            {
+                UserCredential credential;
+
+                using (var stream = new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+                {
+                    string credPath = "token.json";
+                    credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                        GoogleClientSecrets.Load(stream).Secrets,
+                        Scopes,
+                        "user",
+                        CancellationToken.None,
+                        new FileDataStore(credPath, true));
+                }
+
+                var service = new FitnessService(new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = ApplicationName,
+                });
+
+                // Define the request.
+                var request = service.Users.Dataset.Aggregate("me", new AggregateRequest
+                {
+                    AggregateBy = new List<AggregateBy>
+                    {
+                        new AggregateBy
+                        {
+                            DataSourceId = "derived:com.google.step_count.delta:com.google.android.gms:estimated_steps",
+                        },
+                    },
+                    BucketByTime = new BucketByTime
+                    {
+                        DurationMillis = (long)TimeSpan.FromDays(1).TotalMilliseconds,
+                    },
+                    StartTimeMillis = ((DateTimeOffset)startDate).ToUnixTimeMilliseconds(),
+                    EndTimeMillis = ((DateTimeOffset)endDate).ToUnixTimeMilliseconds(),
+                });
+
+                // Fetch the data.
+                var response = await request.ExecuteAsync();
+                int stepCount = 0;
+
+                foreach (var bucket in response.Bucket)
+                {
+                    foreach (var dataset in bucket.Dataset)
+                    {
+                        foreach (var point in dataset.Point)
+                        {
+                            foreach (var value in point.Value)
+                            {
+                                if (value.Key == "intVal")
+                                {
+                                    stepCount += Convert.ToInt32(value.Value[0]);
+                                }
+                            }
+                        }
+                    }
+                }
+
+                return stepCount;
+            }
+
+
+            private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+            {
+
+            }
+        }
+    }
+}
